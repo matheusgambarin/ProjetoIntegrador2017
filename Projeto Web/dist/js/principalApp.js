e1e04656b63374c97c870c8377ffe9abc1cc2273
DESKTOP-57QE3ET\Matheus <matheus-gbn@hotmail.com>
2017-08-06 21:46:34 -0300

    Corrigindo rotas

--- Projeto Web/dist/js/principalApp.js
+++ Projeto Web/dist/js/principalApp.js
@@ -1,53 +1,53 @@
-var app = angular.module("indexApp", ["ngRoute"]);
-app.config(function($routeProvider) {
+var app = angular.module("AdoteUmPet", ["ngRoute"]);
+
+
+app.config(function($routeProvider, $locationProvider) {
     $routeProvider
-    .when("/", {
-        templateUrl : "view/index.html",
-        controller :  "indexCTRL"
-    })
+//     .when("/", {
+//         templateUrl : "index.html"
+//     })
     .when("/adocao", {
-        templateUrl : "view/adocao.html",
-        controller :  "adocaoCTRL"
+        templateUrl : "view/adocao.html"
     })
-    .when("/achados", {
-        templateUrl : "view/achados.html",
-        controller :  "achadosCTRL"
-    })
-    .when("/vendas", {
-        templateUrl : "view/vendas.htm.",
-        controller :  "vendasCTRL"
-    })
-    .when("/produtos", {
-        templateUrl : "view/produtos.html",
-        controller :  "produtosCTRL"
-    })
-    .when("/anuncios", {
-        templateUrl : "view/sAnuncios.html",
-        controller :  "sAnunciosCTRL"
-    })
-    .when("/configuracao", {
-        templateUrl : "view/configuracao.html",
-        controller :  "configuracaoCTRL"
-    })
-    .controller("indexCTRL", function($scope){
-
-    })
-    .controller("adocaoCTRL", function($scope){
-
-    })
-    .controller("achadosCTRL", function($scope){
-
-    })
-    .controller("vendasCTRL", function($scope){
-
-    })
-    .controller("produtosCTRL", function($scope){
-
-    })
-    .controller("anunciosCTRL", function($scope){
-
-    })
-    .controller("configuracaoCTRL", function($scope){
-
-    })
+//     .when("/achados", {
+//         templateUrl : "view/achados.html",
+//         controller :  "achadosCTRL"
+//     })
+//     .when("/vendas", {
+//         templateUrl : "view/vendas.htm.",
+//         controller :  "vendasCTRL"
+//     })
+//     .when("/produtos", {
+//         templateUrl : "view/produtos.html",
+//         controller :  "produtosCTRL"
+//     })
+//     .when("/anuncios", {
+//         templateUrl : "view/sAnuncios.html",
+//         controller :  "sAnunciosCTRL"
+//     })
+//     .when("/configuracao", {
+//         templateUrl : "view/configuracao.html",
+//         controller :  "configuracaoCTRL"
+//     });
 });
+    // .controller("indexCTRL", function($scope){
+
+    // })
+    // .controller("adocaoCTRL", function($scope){
+
+    // })
+    // .controller("achadosCTRL", function($scope){
+
+    // })
+    // .controller("vendasCTRL", function($scope){
+
+    // })
+    // .controller("produtosCTRL", function($scope){
+
+    // })
+    // .controller("anunciosCTRL", function($scope){
+
+    // })
+    // .controller("configuracaoCTRL", function($scope){
+
+    // })
