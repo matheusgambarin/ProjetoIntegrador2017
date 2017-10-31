68502a338eeb5bd8bb092d87da8391eade3d5873
LAPTOP-LN0D4HN8\Edu_D <edu_drozda@hotmail.com>
2017-10-31 18:39:47 -0200

    Criado a tela de dados pessoais

--- Projeto Web/view/configuracao/configuracao.js
+++ Projeto Web/view/configuracao/configuracao.js
@@ -1,7 +1,27 @@
 app.controller("configuracaoCTRL", function ($scope, $http) {
 
-    $scope.configuracaoView = { 
-        nome: "Finin", email: "finin@gmail.com", telefone: "99955820", bairro: "jd aline", complemento: "casa A", numero: "20", rua: "jaboticabal", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500" }
+    $scope.destravaSenha = false;
+    $scope.label = "Mudar a senha";
+    $scope.configuracaoView = {
+        nome: "Finin", email: "finin@gmail.com", telefone: "44999857452", senha: "123456qwe", fotoUrl: "http://placehold.it/350x350"
+    }
 
+    $scope.senha = $scope.configuracaoView.senha;
+    $scope.mudarSenha = function(){
+        if($scope.destravaSenha == false){
+            $scope.destravaSenha = true;
+            $scope.senha = null;
+            $scope.label = "Cancelar";
+        }else{
+            $scope.destravaSenha = false;
+            $scope.senha = $scope.configuracaoView.senha;
+            $scope.label = "Mudar a senha";
+        }
+    }
+    
+    $scope.alterarDadosPessoais = function(){
+        $scope.configuracaoView.senha = $scope.senha;
+        console.log($scope.configuracaoView);
+    }
 
 })
