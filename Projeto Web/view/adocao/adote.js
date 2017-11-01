21a8836edab88d1e85f4bc61a7f919546d9f4c02
Matheus Gambarin <matheus-gbn@hotmail.com>
2017-11-01 21:27:05 -0200

    inicio integracao

--- Projeto Web/view/adocao/adote.js
+++ Projeto Web/view/adocao/adote.js
@@ -1,58 +1,22 @@
 app.controller("adoteCTRL", function($scope, $http){
+	
+	var url = "http://localhost:9090/animal/adocao?field="
+
 	$scope.photoUrl = "http://placehold.it/900x900";
-	$scope.adoteView = [
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Paraná", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Brasil", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Brasil", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Brasil", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Brasil", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Brasil", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Brasil", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Brasil", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Brasil", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Brasil", fotoUrl: "http://placehold.it/750x500"},
-		{nome: "Cachorro", cidade: "Maringá", estado: "Brasil", fotoUrl: "http://placehold.it/750x500"}
-	];
+	$scope.animaisAdocao = [];
+
+
+	$scope.init = function() {
+		$http.get(url)
+		.then(function(response) {
+			console.log(response);
+		});
+	}
+
 	$scope.salvarAnuncio = function(adocao){
 		console.log(adocao);
 	}
+
 	$scope.carregarFoto = function(foto){
 		$scope.photoUrl = foto;
 		console.log(foto);
